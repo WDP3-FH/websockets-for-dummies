e0e5c45b472d1443a8c99f2d6c5f6f0172344297
Jan Donnerbauer <s2210307056@students.fh-hagenberg.at>
2023-12-20 15:21:02 +0100

    fixed header/footer + colors

--- summary_page/scripts/index.js
+++ summary_page/scripts/index.js
@@ -1,30 +1,42 @@
 /*----------  ScrollSpy  ----------*/
 
 window.onload = () => {
-  const observer = new IntersectionObserver((entries) => {
-    // entries are all the sections with an id
-    entries.forEach((entry) => {
-      const id = entry.target.getAttribute("id");
+    const observer = new IntersectionObserver((entries) => {
+        // entries are all the sections with an id
+        entries.forEach((entry) => {
+            const id = entry.target.getAttribute("id");
 
-      /*
+            /*
       When the section is in the viewport,
         add the active class to the corresponding nav link,
         otherwise remove it
       */
-      if (entry.isIntersecting) {
-        document
-          .querySelector(`.scrollspy-nav li a[href="#${id}"]`)
-          .classList.add("active");
-      } else {
-        document
-          .querySelector(`.scrollspy-nav li a[href="#${id}"]`)
-          .classList.remove("active");
-      }
+            if (entry.isIntersecting) {
+                document
+                    .querySelector(`.scrollspy-nav li a[href="#${id}"]`)
+                    .classList.add("active");
+            } else {
+                document
+                    .querySelector(`.scrollspy-nav li a[href="#${id}"]`)
+                    .classList.remove("active");
+            }
+        });
     });
-  });
 
-  // Add the observer to each section with an id
-  document.querySelectorAll("section[id]").forEach((section) => {
-    observer.observe(section);
-  });
+    // Add the observer to each section with an id
+    document.querySelectorAll("section[id]").forEach((section) => {
+        observer.observe(section);
+    });
+
+    //darkmode-toggle
+    document
+        .getElementsByClassName("js-darkmode-toggle")[0]
+        .addEventListener("click", () => {
+            document
+                .getElementsByClassName("js-darkmode-sun")[0]
+                .classList.toggle("hidden");
+            document
+                .getElementsByClassName("js-darkmode-moon")[0]
+                .classList.toggle("hidden");
+        });
 };
