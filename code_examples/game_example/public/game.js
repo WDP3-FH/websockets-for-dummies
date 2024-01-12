c69de851804099b8cab6b8c19a470cdc889032b8
Elismasher <rist24082001@gmail.com>
2024-01-12 20:01:16 +0100

    add shadow to own user

--- code_examples/game_example/public/game.js
+++ code_examples/game_example/public/game.js
@@ -2,8 +2,8 @@
 var config = {
   type: Phaser.AUTO,
   parent: "phaser-example",
-  width: 500,
-  height: 500,
+  width: 960,
+  height: 640,
   zoom: 1,
   pixelArt: true,
   scene: {
@@ -13,9 +13,13 @@ var config = {
   },
 };
 
+const movementSpeed = 16;
+const shadowOffset = 10;
+
 var game = new Phaser.Game(config);
 
 function preload() {
+  this.load.image("background", "assets/map/first_map.png");
   this.load.image("ship_blue", "assets/kenney_pixel-shmup/Ships/ship_0000.png");
   this.load.image("ship_red", "assets/kenney_pixel-shmup/Ships/ship_0001.png");
   this.load.image(
@@ -33,6 +37,12 @@ function preload() {
 }
 
 function create() {
+  //Set background
+  let background = this.add.sprite(0, 0, "background");
+  background.setOrigin(0, 0);
+  background.displayWidth = this.sys.game.config.width;
+  background.displayHeight = this.sys.game.config.height;
+
   var self = this;
   this.socket = io();
   this.otherPlayers = [];
@@ -77,13 +87,17 @@ function create() {
 function update() {
   if (this.player_ship) {
     if (this.input.keyboard.checkDown(this.cursors.left, 250)) {
-      this.player_ship.x -= 16;
+      this.player_ship.x -= movementSpeed;
+      this.player_ship_shadow.x -= movementSpeed;
     } else if (this.input.keyboard.checkDown(this.cursors.right, 250)) {
-      this.player_ship.x += 16;
+      this.player_ship.x += movementSpeed;
+      this.player_ship_shadow.x += movementSpeed;
     } else if (this.input.keyboard.checkDown(this.cursors.down, 250)) {
-      this.player_ship.y += 16;
+      this.player_ship.y += movementSpeed;
+      this.player_ship_shadow.y += movementSpeed;
     } else if (this.input.keyboard.checkDown(this.cursors.up, 250)) {
-      this.player_ship.y -= 16;
+      this.player_ship.y -= movementSpeed;
+      this.player_ship_shadow.y -= movementSpeed;
     }
     //emit player movement
     var x = this.player_ship.x;
@@ -110,6 +124,9 @@ function addPlayer(self, playerInfo) {
   self.player_ship = self.add
     .sprite(playerInfo.x, playerInfo.y, playerInfo.sprite)
     .setOrigin(0.5, 0.5);
+  self.player_ship.depth = 10;
+
+  self.player_ship_shadow = createShadow(self, playerInfo);
 }
 
 function addOtherPlayers(self, playerInfo) {
@@ -118,4 +135,20 @@ function addOtherPlayers(self, playerInfo) {
     .setOrigin(0.5, 0.5);
   otherPlayer.playerId = playerInfo.playerId;
   self.otherPlayers.push(otherPlayer);
+}
+
+function createShadow(self, playerInfo) {
+  const shadow = self.add
+    .sprite(playerInfo.x, playerInfo.y, playerInfo.sprite)
+    .setOrigin(0.5, 0.5);
+
+  shadow.tint = 0x000000;
+  shadow.alpha = 0.15;
+  shadow.depth = 9;
+
+  shadow.x -= shadowOffset;
+  shadow.y += shadowOffset;
+  shadow.setScale(0.95);
+
+  return shadow;
 }
